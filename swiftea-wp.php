1dd360872609713adc6a58c92e17e463df384790
Hugo Posnic <hposnic@altais.fr>
2020-02-23 11:41:14 +0100

    Initial plugin structure

---
+++ swiftea-wp.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Swiftea
+ * Plugin URI: https://swifteasearch.alwaysdata.net
+ * Description: Powerful internal search for your website
+ * Version: 0.1
+ * Author: Hugo Posnic, Nathan Seva
+ * Author URI: https://swifteasearch.alwaysdata.net
+ */
